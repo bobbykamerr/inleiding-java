e0f948512a84215e13116bc6c900e7102ac618a5
bobbykamerr <bobby.kamer@student.rocvf.nl>
2020-09-09 14:49:42 +0200

    m

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("text test!");
+        System.out.println("text -test!");
     }
 }
