1b59883d4fc734c2194ba45b322e37e7b739788f
bobbykamerr <bobby.kamer@student.rocvf.nl>
2020-09-29 13:06:52 +0200

    Bezig met overzetten bestanden.

---
+++ src/h04/Opdracht1.java
@@ -0,0 +1,16 @@
+package h04;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht1 extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        g.drawLine(50, 120, 150, 50);
+        g.drawLine(150,50,240, 120);
+        g.drawLine(50, 120, 240, 120);
+    }
+}
