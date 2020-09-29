26959d9545da1ee72905d4d7ad5cb15f3d554671
bobbykamerr <bobby.kamer@student.rocvf.nl>
2020-09-29 13:39:22 +0200

    vorm maken van het rode stuk

---
+++ src/h04/Opdracht3.java
@@ -0,0 +1,14 @@
+package h04;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht3 extends Applet {
+
+    public void init()  {
+    }
+    public void paint(Graphics g) {
+        g.drawRect(30, 120,20,300);
+        g.drawRect(50,120,200,30);
+    }
+}
