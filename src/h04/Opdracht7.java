38c14563aa71ee048bce608c1713853bc1540d99
bobbykamerr <bobby.kamer@student.rocvf.nl>
2020-09-30 13:14:32 +0200

    dobbelsteen af

---
+++ src/h04/Opdracht7.java
@@ -0,0 +1,16 @@
+package h04;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht7 extends Applet {
+    public void init (){
+    }
+    public void paint (Graphics g){
+        g.drawRoundRect(10,10,70,70,50,50);
+        g.fillOval(25,25,15,15);
+        g.fillOval(25,50,15,15);
+        g.fillOval(50,25,15,15);
+        g.fillOval(50,50,15,15);
+    }
+}
