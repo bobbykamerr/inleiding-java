41fc2b2114597d0ea3bc49fdbd98aa7e1c5528ca
bobbykamerr <bobby.kamer@student.rocvf.nl>
2020-10-07 12:57:34 +0200

    .

--- src/h04/Opdracht1.java
+++ src/h04/Opdracht1.java
@@ -9,7 +9,7 @@ public class Opdracht1 extends Applet {
     }
 
     public void paint(Graphics g) {
-        g.drawLine(50, 120, 150, 50);
+        g.drawLine(49, 120, 150, 50);
         g.drawLine(150,50,240, 120);
         g.drawLine(50, 120, 240, 120);
     }
